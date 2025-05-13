61b87471f0cad9effe7302f8937cb493bfdf5a71
Movsum <115896904+movsumhasanov@users.noreply.github.com>
2025-05-13 14:10:51 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "general-sans": ["General Sans", "sans-serif"],
+      },
+      colors: {
+        green: {
+          50: "#30AF5B",
+          90: "#292C27",
+        },
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
+        },
+        orange: {
+          50: "#FF814C",
+        },
+        blue: {
+          70: "#021639",
+        },
+        yellow: {
+          50: "#FEC601",
+        },
+      },
+      screens: {
+        xs: "480px",
+        cLg: "992px",
+        "2xl": "1440px",
+        "3xl": "1600px",
+        fullHD: "1920px",
+        "4xl": "2200px",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "newsletter-form": 'url("/newsletter-element.svg")',
+      },
+      gridTemplateColumns: {
+        "custom-columns": "auto auto 1fr",
+      },
+    },
+  },
+  plugins: [],
+};
